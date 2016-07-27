d93cf72bdb5e21a92dc54beb837ac9cf32c23d29
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-07-27 08:24:10 -0700

    Add variables.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,16 @@
 public class HelloWorld {
-    public static void main(String args[]) {
+	public static void main(String args[]) {
         // print string
-        System.out.println("Hello World!");
+		System.out.println("Hello World!");
         //print integer
-        System.out.println(10);
+		System.out.println(10);
         //print char
-        System.out.println('*');
-        System.out.println('K');
+		System.out.println('*');
+		System.out.println('K');
 
-    }
+		int myNumber = 42;
+		boolean isFun = true; 
+		char wordRating = 'A'; 
+
+	}
 }
