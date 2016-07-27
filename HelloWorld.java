b38020b2ed2b6405fa83edf7c2b9cc8b316b9f68
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-07-27 08:33:14 -0700

    Basic arithmetic and equality.

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,9 +8,20 @@ public class HelloWorld {
 		System.out.println('*');
 		System.out.println('K');
 
+		// define type of variable and then variable name
 		int myNumber = 42;
 		boolean isFun = true; 
 		char wordRating = 'A'; 
 
+		// basic arithmetic
+		int sum = 34 + 113;
+		int difference = 91 - 205;
+		int product = 2 * 8; 
+		int quotient = 45 / 3;
+
+		System.out.println(myNumber == sum);
+		System.out.println(difference != product);
+
+		
 	}
 }
