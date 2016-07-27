9724010e97e0a674388fbc9e31025b1ce4d5037e
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-07-27 13:04:32 -0700

    Add ArrayLists.

--- Temperatures.java
+++ Temperatures.java
@@ -3,7 +3,23 @@ import java.util.ArrayList;
 public class Temperatures {
 	
 	public static void main(String[] args) {
-		ArrayList<Integer> weeklyTemperatures = new
-		ArrayList<Integer>();
+
+		ArrayList<Integer> weeklyTemperatures = new ArrayList<Integer>();
+		weeklyTemperatures.add(78);
+		weeklyTemperatures.add(67);
+		weeklyTemperatures.add(89);
+		weeklyTemperatures.add(94);
+
+		weeklyTemperatures.add(2, 111);
+		System.out.println( weeklyTemperatures.get(3) );
+
+		for (int j = 0; j < weeklyTemperatures.size(); j++){
+			System.out.println(weeklyTemperatures.get(j) );
+		}
+
+		// Alternatively can be written:
+		for (Integer temperature : weeklyTemperatures) {
+			System.out.println(temperature);
+		}
 	}
 }
